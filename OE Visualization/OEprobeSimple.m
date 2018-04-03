a018a04de4d5415bec73180635e8ebbdb8b302ce
gaelias <gelias@uci.edu>
2018-04-02 18:54:49 -0700

    BPF & Raw options
    Added in section to plot bandpass filtered trace in addition to the raw trace

--- OE Visualization/OEprobeSimple.m
+++ OE Visualization/OEprobeSimple.m
@@ -1,3 +1,23 @@
+%% For RAW trace
+t = 0;
+for tet = 1:4:64
+    t = t+1;
+    [data1, ~, ~] = load_open_ephys_data(sprintf('100_CH%i.continuous', tet));
+    [data2, ~, ~] = load_open_ephys_data(sprintf('100_CH%i.continuous', tet+1));
+    [data3, ~, ~] = load_open_ephys_data(sprintf('100_CH%i.continuous', tet+2));
+    [data4, ~, ~] = load_open_ephys_data(sprintf('100_CH%i.continuous', tet+3));
+    
+    figure;
+    plot(data1);
+    hold on;
+    plot(data2);
+    plot(data3);
+    plot(data4);
+%     set(gca, 'xlim', z);
+    title(sprintf('Probe%i Raw', t));
+    drawnow
+end
+%% For Bandpass Filtered traces
 t = 0;
 for tet = 1:4:64
     t = t+1;
@@ -13,6 +33,6 @@ for tet = 1:4:64
     plot(data3);
     plot(data4);
 %     set(gca, 'xlim', z);
-    title(sprintf('Probe%i', t));
+    title(sprintf('Probe%i Filtered', t));
     drawnow
 end
