c3a15db298fdce965627c639758d5ba5670809d0
gaelias <gelias@uci.edu>
2019-04-04 11:55:04 -0700

    Update PlotLineAndFilledError.m

--- PlotLineAndFilledError.m
+++ PlotLineAndFilledError.m
@@ -3,6 +3,12 @@ function [meanAx] = PlotLineAndFilledError(xVals, lineMean, lineVar, color)
 %
 %
 %%
-jbfill(xVals,lineMean+lineVar,lineMean-lineVar,color,color,0,.2);
-hold on;
+if diff(size(xVals))<0
+    xVals = xVals';
+end
 meanAx = plot(xVals, lineMean, 'color', color);
+hold on;
+patch(gca, 'XData', [xVals, fliplr(xVals)],...
+    'YData', [lineMean+lineVar, fliplr(lineMean+lineVar)],...
+    'FaceColor', color, 'FaceAlpha', 0.15,...
+    'EdgeColor', color);
